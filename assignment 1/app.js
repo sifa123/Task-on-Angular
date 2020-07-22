e5206b10feac1d3375b0353154fd22a196a1bf2a
Sefat Siddiquea Sifa <63462931+sifa123@users.noreply.github.com>
2020-07-22 14:02:48 +0600

    Update app.js

--- assignment 1/app.js
+++ assignment 1/app.js
@@ -7,10 +7,29 @@ angular.module('MsgApp', [])
 MsgController.$inject = ['$scope'];
 
 
- $scope.chicken = function () {
-    $scope.stateOfBeing = "fed";
-    
-  };
+function MsgController($scope) {
+        $scope.dishes = '';
+        $scope.message = '';
+        $scope.checked = false;
+
+        $scope.checkLunch = function() {
+            if ($scope.dishes.trim().length === 0) {
+                $scope.empty = true;
+            } else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var arrayDishes = $scope.dishes.split(',');
+                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+                    return v.trim() !== '';
+                });
+
+                if (arrayDishesWithoutEmptys.length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
+                }
+            }
+        };
 }
 
 
