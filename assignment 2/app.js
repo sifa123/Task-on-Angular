4e6184d530dcd7dbf670310fb78af21c9c93a3f4
Sefat Siddiquea Sifa <63462931+sifa123@users.noreply.github.com>
2020-08-10 16:25:41 +0600

    Update app.js

--- assignment 2/app.js
+++ assignment 2/app.js
@@ -1,10 +1,58 @@
-(function(){
+(function () {
   'use strict';
+  
+  angular.module('BuyLists', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-angular.module('myApp', [])
-  .controller('myCtrl', ['$scope', function($scope) {
-    $scope.count = 0;
-    $scope.myFunc = function() {
-      $scope.count++;
-    };
- });
+  ToBuyController.$inject = ["ShoppingListCheckOffService"];
+  function ToBuyController (ShoppingListCheckOffService) {
+    let list1 = this;
+
+    list1.items = ShoppingListCheckOffService.getToBuy();
+    list1.isEmpty = false;
+    list1.bought = function (itemIndex) {
+      list1.isEmpty = ShoppingListCheckOffService.buy(itemIndex);
+    }
+    
+  }
+
+
+  AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"];
+  function AlreadyBoughtController (ShoppingListCheckOffService) {
+    let list2 = this;
+    list2.items = ShoppingListCheckOffService.getBought();
+    list2.isEmpty = function() {
+      return list2.items.length == 0;
+    }
+      
+  }
+
+  function ShoppingListCheckOffService () {
+    let service = this;
+    let itemsToBuy = [{name: "ss", quantity: 1 },
+                      {name: "dd", quantity: 2 },
+                      {name: "ff", quantity: 2 },
+                      {name: "gg", quantity: 1 },
+                      {name: "aa", quantity: 1 }];
+    let itemsBought = [];
+
+    service.getToBuy = function () {
+      return itemsToBuy;
+
+    }
+
+    service.getBought = function () {
+      return itemsBought;
+    }
+
+    service.buy = function (itemIndex) {
+      itemsBought.push(itemsToBuy[itemIndex]);
+      itemsToBuy.splice(itemIndex, 1);
+      return itemsToBuy.length == 0;
+    }
+
+  }
+
+})();
